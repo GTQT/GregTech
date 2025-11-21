c68c56762bab8c4de2deecf748ebfe653ac3deb6
Dr-ppp <672749065@qq.com>
2025-11-21 10:05:56 +0800

    虚拟模具槽  未经测试

---
+++ src/main/java/gregtech/api/capability/impl/GhostMouldItemStackHandler.java
@@ -0,0 +1,198 @@
+package gregtech.api.capability.impl;
+
+import gregtech.api.capability.INotifiableHandler;
+import gregtech.api.items.itemhandlers.GTItemStackHandler;
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.recipes.ingredients.IntCircuitIngredient;
+
+import gregtech.common.items.MetaItems;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.items.IItemHandlerModifiable;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GhostMouldItemStackHandler extends GTItemStackHandler
+                                          implements IItemHandlerModifiable, INotifiableHandler {
+
+    /**
+     *特殊电路值表示不设置电路值。
+     * */
+    public static final int NO_CONFIG = -1;
+
+    private final List<MetaTileEntity> notifiableEntities = new ArrayList<>();
+
+    private int circuitValue = NO_CONFIG;
+    private ItemStack circuitStack = ItemStack.EMPTY;
+
+    public GhostMouldItemStackHandler(MetaTileEntity metaTileEntity) {
+        super(metaTileEntity);
+    }
+
+    /**
+     *返回电路值，或{@link GhostMouldItemStackHandler#NO_CONFIG}如果
+    *无电路值设置。
+     ＊
+     * @返回一个有效的电路值，如果存在，否则{@link GhostMouldItemStackHandler#NO_CONFIG}
+    */
+    public int getCircuitValue() {
+        return this.circuitValue;
+    }
+
+    /**
+     *返回该实例是否包含有效的电路值。
+     ＊
+     * @return此实例是否包含有效的电路值。
+     */
+    public boolean hasCircuitValue() {
+        return this.circuitValue != NO_CONFIG;
+    }
+
+    /**
+     * 将此库存的电路值设置为给定值并更新物品。物品被设置为对应整数值的电路物品，
+     * 或如果给定 {@link GhostMouldItemStackHandler#NO_CONFIG} 则为一个空的物品堆栈。
+     * <p>
+     * 该值预期为有效的电路值
+     * ({@link IntCircuitIngredient#CIRCUIT_MIN} ~ {@link IntCircuitIngredient#CIRCUIT_MAX}，包括两者)
+     * 或 {@link GhostMouldItemStackHandler#NO_CONFIG}；任何其他值将产生 IllegalArgumentException。
+     *
+     * @param config 新的配置值
+     * @throws IllegalArgumentException 输入无效时抛出
+     */
+
+    public void setCircuitValue(int config) {
+        if (config == NO_CONFIG) {
+            this.circuitValue = NO_CONFIG;
+            this.circuitStack = ItemStack.EMPTY;
+        } else if (config >= IntCircuitIngredient.CIRCUIT_MIN && config < MetaItems.SHAPE_MOLDS.length) {
+            this.circuitValue = config;
+            this.circuitStack = MetaItems.SHAPE_MOLDS[config].getStackForm();
+        } else {
+            throw new IllegalArgumentException("Circuit value out of range: " + config);
+        }
+        for (MetaTileEntity mte : notifiableEntities) {
+            if (mte != null && mte.isValid()) {
+                addToNotifiedList(mte, this, false);
+            }
+        }
+    }
+
+    /**
+     * 从给定物品设置此库存的电路值。只有当提供的物品为整数电路时，电路值才会被设置为有效的电路值；
+     * 提供任何其他物品将把电路值设置为 {@link GhostMouldItemStackHandler#NO_CONFIG}。
+     *
+     * @param stack 要读取电路值的物品堆栈
+     */
+
+    public void setCircuitValueFromStack(@NotNull ItemStack stack) {
+        if(!stack.isEmpty())
+        {
+            if(stack.getItem()==MetaItems.SHAPE_MOLDS[0].getStackForm().getItem() &&
+                    stack.getMetadata() >= MetaItems.SHAPE_MOLDS[0].getMetaValue() &&
+                    stack.getMetadata() <= MetaItems.SHAPE_MOLDS[MetaItems.SHAPE_MOLDS.length].getMetaValue()
+            )
+            {
+                for (int i = 0; i < MetaItems.SHAPE_MOLDS.length; i++) {
+                    if(stack.getMetadata()==MetaItems.SHAPE_MOLDS[i].getMetaValue())
+                    {
+                        setCircuitValue(i);
+                        break;
+                    }
+                }
+            }
+        }else
+        {
+            setCircuitValue(NO_CONFIG);
+        }
+    }
+
+    /**
+     * 将给定值添加到现有的电路值中。结果值被限制在有效电路值的范围内。
+     * 如果没有电路值存在，则此方法不执行任何操作。
+     *
+     * @param configDelta 要添加的电路值，可以是负数
+     */
+    public void addCircuitValue(int configDelta) {
+        if (hasCircuitValue()) {
+            setCircuitValue(MathHelper.clamp(getCircuitValue() + configDelta,
+                    IntCircuitIngredient.CIRCUIT_MIN, MetaItems.SHAPE_MOLDS.length));
+        }
+    }
+
+    @Override
+    public void setStackInSlot(int slot, @NotNull ItemStack stack) {
+        validateSlot(slot);
+        setCircuitValueFromStack(stack);
+    }
+
+    @Override
+    public int getSlots() {
+        return 1;
+    }
+
+    @NotNull
+    @Override
+    public ItemStack getStackInSlot(int slot) {
+        validateSlot(slot);
+        return this.circuitStack;
+    }
+
+    @NotNull
+    @Override
+    public ItemStack insertItem(int slot, @NotNull ItemStack stack, boolean simulate) {
+        validateSlot(slot);
+        return stack; // reject all item insertions
+    }
+
+    @NotNull
+    @Override
+    public ItemStack extractItem(int slot, int amount, boolean simulate) {
+        if (amount <= 0) return ItemStack.EMPTY;
+        validateSlot(slot);
+        if (!simulate) {
+            setCircuitValue(NO_CONFIG);
+        }
+        return this.circuitStack;
+    }
+
+    @Override
+    public int getSlotLimit(int slot) {
+        validateSlot(slot);
+        return 1;
+    }
+
+    protected void validateSlot(int slot) {
+        if (slot != 0) throw new IndexOutOfBoundsException("Slot index out of bounds: " + slot);
+    }
+
+    @Override
+    public void addNotifiableMetaTileEntity(MetaTileEntity metaTileEntity) {
+        if (metaTileEntity == null) return;
+        this.notifiableEntities.add(metaTileEntity);
+    }
+
+    @Override
+    public void removeNotifiableMetaTileEntity(MetaTileEntity metaTileEntity) {
+        this.notifiableEntities.remove(metaTileEntity);
+    }
+
+    public void write(@NotNull NBTTagCompound tag) {
+        if (this.circuitValue != NO_CONFIG) {
+            tag.setByte("GhostMould", (byte) this.circuitValue);
+        }
+    }
+
+    public void read(@NotNull NBTTagCompound tag) {
+        int circuitValue = tag.hasKey("GhostMould", Constants.NBT.TAG_ANY_NUMERIC) ? tag.getInteger("GhostMould") :
+                NO_CONFIG;
+        if (circuitValue < IntCircuitIngredient.CIRCUIT_MIN || circuitValue > MetaItems.SHAPE_MOLDS.length)
+            circuitValue = NO_CONFIG;
+        setCircuitValue(circuitValue);
+    }
+}
---
+++ src/main/java/gregtech/api/mui/widget/GhostMouldSlotWidget.java
@@ -0,0 +1,246 @@
+package gregtech.api.mui.widget;
+
+import com.cleanroommc.modularui.api.IPanelHandler;
+import com.cleanroommc.modularui.api.UpOrDown;
+import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.api.widget.Interactable;
+import com.cleanroommc.modularui.drawable.ItemDrawable;
+import com.cleanroommc.modularui.integration.recipeviewer.RecipeViewerIngredientProvider;
+import com.cleanroommc.modularui.screen.RichTooltip;
+import com.cleanroommc.modularui.utils.Alignment;
+import com.cleanroommc.modularui.utils.MouseData;
+import com.cleanroommc.modularui.value.sync.SyncHandler;
+import com.cleanroommc.modularui.widget.Widget;
+import com.cleanroommc.modularui.widgets.ButtonWidget;
+import com.cleanroommc.modularui.widgets.layout.Grid;
+
+import gregtech.api.capability.impl.GhostMouldItemStackHandler;
+import gregtech.api.capability.impl.GhostMouldItemStackHandler;
+import gregtech.api.mui.GTGuiTextures;
+import gregtech.api.mui.GTGuis;
+import gregtech.api.recipes.ingredients.IntCircuitIngredient;
+import gregtech.client.utils.TooltipHelper;
+
+import gregtech.common.items.MetaItems;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.items.IItemHandlerModifiable;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+
+public class GhostMouldSlotWidget extends Widget<GhostMouldSlotWidget> implements Interactable,
+                                                                                      RecipeViewerIngredientProvider {
+
+    private static final int SYNC_CIRCUIT_INDEX = 10;
+    private static final int SYNC_SCROLL = 3;
+    private static final int SYNC_CLICK = 2;
+    private static final int SYNC_CIRCUIT = 1;
+
+    @Nullable
+    private IPanelHandler selectorPanel;
+    private GhostMouldSyncHandler syncHandler;
+
+    public GhostMouldSlotWidget() {
+        tooltipAutoUpdate(true);
+        tooltipBuilder(this::getCircuitSlotTooltip);
+        size(18);
+        background(GTGuiTextures.SLOT, GTGuiTextures.INT_CIRCUIT_OVERLAY);
+        ItemDrawable stack = new ItemDrawable();
+        overlay(stack.asIcon().alignment(Alignment.Center));
+        onUpdateListener(w -> stack.setItem(syncHandler.getCircuitStack()));
+    }
+
+    @Override
+    public @NotNull Result onMousePressed(int mouseButton) {
+        if (!isSelectorPanelOpen()) {
+            if (mouseButton == 0 && TooltipHelper.isShiftDown()) {
+                this.getSelectorPanel().openPanel();
+            } else {
+                MouseData mouseData = MouseData.create(mouseButton);
+                getSyncHandler().syncToServer(SYNC_CLICK, mouseData::writeToPacket);
+            }
+        }
+        return Result.SUCCESS;
+    }
+
+    @Override
+    public boolean onMouseScroll(UpOrDown scrollDirection, int amount) {
+        if (isSelectorPanelOpen()) return true;
+        MouseData mouseData = MouseData.create(scrollDirection.modifier);
+        getSyncHandler().syncToServer(SYNC_SCROLL, mouseData::writeToPacket);
+        return true;
+    }
+
+    @Override
+    public boolean isValidSyncHandler(SyncHandler syncHandler) {
+        this.syncHandler = castIfTypeElseNull(syncHandler, GhostMouldSyncHandler.class);
+        return this.syncHandler != null;
+    }
+
+    public GhostMouldSlotWidget slot(IItemHandlerModifiable handlerModifiable, int index) {
+        this.syncHandler = new GhostMouldSyncHandler(handlerModifiable, index);
+        isValidSyncHandler(this.syncHandler);
+        setSyncHandler(this.syncHandler);
+        return this;
+    }
+
+    protected void getCircuitSlotTooltip(@NotNull RichTooltip tooltip) {
+        String configString;
+        int value = this.syncHandler.getCircuitValue();
+        if (value == GhostMouldItemStackHandler.NO_CONFIG) {
+            configString = IKey.lang("gregtech.gui.configurator_slot.no_value").get();
+        } else {
+            configString = String.valueOf(value);
+        }
+        tooltip.clearText();
+        tooltip.addLine(IKey.lang("gregtech.gui.configurator_slot.tooltip", configString));
+    }
+
+    @Override
+    public boolean onMouseRelease(int mouseButton) {
+        return true;
+    }
+
+    private boolean isSelectorPanelOpen() {
+        return this.getSelectorPanel().isPanelOpen();
+    }
+
+    @NotNull
+    private IPanelHandler getSelectorPanel() {
+        if (this.selectorPanel == null) {
+            this.selectorPanel = IPanelHandler.simple(getPanel(), (mainPanel, player) -> {
+                ItemDrawable circuitPreview = new ItemDrawable(this.syncHandler.getCircuitStack());
+
+                return GTGuis.createPopupPanel("circuit_selector", 176, 120)
+                        .child(IKey.lang("metaitem.circuit.integrated.gui").asWidget().pos(5, 5))
+                        .child(new Widget<>()
+                                .size(18)
+                                .top(19).alignX(0.5f)
+                                .overlay(circuitPreview.asIcon().margin(1))
+                                .background(GTGuiTextures.SLOT, GTGuiTextures.INT_CIRCUIT_OVERLAY))
+                        .child(new Grid()
+                                .left(7).right(7).top(41).height(4 * 18)
+                                .mapTo(9, 33, value -> new ButtonWidget<>()
+                                        .size(18)
+                                        .background(GTGuiTextures.SLOT, new ItemDrawable(
+                                                MetaItems.SHAPE_MOLDS[value].getStackForm()).asIcon())
+                                        .disableHoverBackground()
+                                        .onMousePressed(mouseButton -> {
+                                            getSyncHandler().syncToServer(SYNC_CIRCUIT_INDEX,
+                                                    buf -> buf.writeShort(value));
+                                            circuitPreview.setItem(MetaItems.SHAPE_MOLDS[value].getStackForm());
+                                            if (Interactable.hasShiftDown()) this.selectorPanel.closePanel();
+                                            return true;
+                                        }))
+                                .minColWidth(18).minRowHeight(18)
+                                .minElementMargin(0, 0));
+            }, true);
+        }
+        return this.selectorPanel;
+    }
+
+    @Override
+    public @Nullable Object getIngredient() {
+        return this.syncHandler.getCircuitStack();
+    }
+
+    private static class GhostMouldSyncHandler extends SyncHandler {
+
+        private final IItemHandlerModifiable handler;
+        private final int index;
+
+        public GhostMouldSyncHandler(IItemHandlerModifiable handler, int index) {
+            if (!(handler instanceof GhostMouldItemStackHandler)) {
+                throw new IllegalArgumentException(
+                        "GhostCircuitSyncHandler has IItemHandler that is not GhostMouldItemStackHandler");
+            }
+            this.handler = handler;
+            this.index = index;
+        }
+
+        @Override
+        public void detectAndSendChanges(boolean init) {
+            if (init) {
+                syncToClient(SYNC_CIRCUIT, buffer -> buffer.writeItemStack(getCircuitStack()));
+            }
+        }
+
+        protected void phantomClick(MouseData mouseData) {
+            if (mouseData.mouseButton == 0) {
+                // increment on left-click
+                setCircuitValue(getNextCircuitValue(1));
+            } else if (mouseData.mouseButton == 1 && mouseData.shift) {
+                // clear on shift-right-click
+                setCircuitValue(GhostMouldItemStackHandler.NO_CONFIG);
+            } else if (mouseData.mouseButton == 1) {
+                // decrement on right-click
+                setCircuitValue(getNextCircuitValue(-1));
+            }
+        }
+
+        protected void phantomScroll(MouseData mouseData) {
+            setCircuitValue(getNextCircuitValue(mouseData.mouseButton));
+        }
+
+        private void setCircuitValue(int value) {
+            GhostMouldItemStackHandler handler = getGhostCircuitHandler();
+            if (handler.getCircuitValue() != value) {
+                handler.setCircuitValue(value);
+                syncToClient(SYNC_CIRCUIT, buf -> buf.writeItemStack(getCircuitStack()));
+            }
+        }
+
+        public int getCircuitValue() {
+            return getGhostCircuitHandler().getCircuitValue();
+        }
+
+        public ItemStack getCircuitStack() {
+            return this.handler.getStackInSlot(this.index);
+        }
+
+        @Override
+        public void readOnClient(int id, PacketBuffer buf) throws IOException {
+            if (id == SYNC_CIRCUIT) {
+                this.handler.setStackInSlot(this.index, buf.readItemStack());
+            }
+        }
+
+        @Override
+        public void readOnServer(int id, PacketBuffer buf) throws IOException {
+            if (id == SYNC_CIRCUIT_INDEX) {
+                setCircuitValue(buf.readShort());
+            } else if (id == SYNC_CLICK) {
+                phantomClick(MouseData.readPacket(buf));
+            } else if (id == SYNC_SCROLL) {
+                phantomScroll(MouseData.readPacket(buf));
+            }
+        }
+
+        private int getNextCircuitValue(int delta) {
+            GhostMouldItemStackHandler handler = getGhostCircuitHandler();
+
+            // if no circuit, skip 0 and return 32 if decrementing,
+            // or, skip 0 and return 1 when incrementing
+            if (!handler.hasCircuitValue()) {
+                return delta == 1 ? 1 : MetaItems.SHAPE_MOLDS.length-1;
+                // if at max, loop around to no circuit
+            } else if (handler.getCircuitValue() + delta >= MetaItems.SHAPE_MOLDS.length) {
+                return GhostMouldItemStackHandler.NO_CONFIG;
+                // if at 1, skip 0 and return to no circuit
+            } else if (handler.getCircuitValue() + delta < 1) {
+                return GhostMouldItemStackHandler.NO_CONFIG;
+            }
+
+            // normal case: change by "delta" which is either 1 or -1
+            return handler.getCircuitValue() + delta;
+        }
+
+        public GhostMouldItemStackHandler getGhostCircuitHandler() {
+            return (GhostMouldItemStackHandler) this.handler;
+        }
+    }
+}
